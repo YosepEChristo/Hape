9f009ab1c90fa92a6e2f3eff6eb156a1ee7ec592
damiosdam <68676609+damiosdam@users.noreply.github.com>
2023-11-21 11:31:58 +0700

    update koroutin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.0")
     annotationProcessor ("androidx.room:room-compiler:2.6.0")
     kapt ("androidx.room:room-compiler:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
 }
