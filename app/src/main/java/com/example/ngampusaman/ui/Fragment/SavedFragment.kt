c7e3779af25d72a681f00e45f9309ad82107bb3e
Yosep E Christo <yosepechristo@gmail.com>
2023-11-22 10:26:32 +0700

    m

--- app/src/main/java/com/example/ngampusaman/ui/Fragment/SavedFragment.kt
+++ app/src/main/java/com/example/ngampusaman/ui/Fragment/SavedFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ngampusaman.R
 
 
@@ -16,5 +18,17 @@ class SavedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saved, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerViewItem = requireView().findViewById<RecyclerView>(R.id.rv_saved)
+        val daftarJudul = arrayOf<String>("Surat Izin Sakit", "Surat Pengunduran Diri", "Surat Ingin Menikah")
+        val daftarKeterangan = arrayOf<String>("Ketika Mahasiswa tidak enak badan dan direkomendasikan pihak medis untuk beristirahat", "Ketika Capek Kuliah DO Solusinya", "Jomblo jangan tekan ini")
+
+        val suratAdapter = SuratAdapter(daftarJudul, daftarKeterangan)
+        recyclerViewItem.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = suratAdapter
+        }
     }
 }
