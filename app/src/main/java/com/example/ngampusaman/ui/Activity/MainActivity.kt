66cc282d2bd82dd23fa19ce47739589fe2b1b436
Yosep E Christo <yosepechristo@gmail.com>
2023-11-26 16:31:20 +0700

    Navigate Home Page to another Fragment

--- app/src/main/java/com/example/ngampusaman/ui/Activity/MainActivity.kt
+++ app/src/main/java/com/example/ngampusaman/ui/Activity/MainActivity.kt
@@ -2,20 +2,26 @@ package com.example.ngampusaman.ui.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ngampusaman.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var  navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
         val navController = Navigation.findNavController(this, R.id.host_fragment)
-
         NavigationUI.setupWithNavController(bottomNavigation,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
     }
 }
--- app/src/main/java/com/example/ngampusaman/ui/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/ngampusaman/ui/Fragment/HomeFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ngampusaman.R
@@ -16,14 +21,69 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val toTemplateFragment: CardView = view.findViewById(R.id.cv_one)
+        toTemplateFragment.setOnClickListener {
+            findNavController()
+                .navigate(
+                    R.id.action_homeFragment_to_templateFragment,
+                    null,
+                    NavOptions.Builder()
+                        .setPopUpTo(R.id.homeFragment, true)
+                        .build()
+                )
+        }
+
+        val toFAQFragment: CardView = view.findViewById(R.id.cv_two)
+        toFAQFragment.setOnClickListener {
+            findNavController()
+                .navigate(
+                    R.id.action_homeFragment_to_faqFragment,
+                    null,
+                    NavOptions.Builder()
+                        .setPopUpTo(R.id.homeFragment, true)
+                        .build()
+                )
+        }
+
+        val toContactUsFragment: CardView = view.findViewById(R.id.cv_three)
+        toContactUsFragment.setOnClickListener {
+            findNavController()
+                .navigate(
+                    R.id.action_homeFragment_to_contactus,
+                    null,
+                    NavOptions.Builder()
+                        .setPopUpTo(R.id.homeFragment, true)
+                        .build()
+                )
+        }
+
+        val toAboutUsFragment: CardView = view.findViewById(R.id.cv_four)
+        toAboutUsFragment.setOnClickListener {
+            findNavController()
+                .navigate(
+                    R.id.action_homeFragment_to_aboutusFragment,
+                    null,
+                    NavOptions.Builder()
+                        .setPopUpTo(R.id.homeFragment, true)
+                        .build()
+                )
+        }
+
+        return view
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerViewItem = requireView().findViewById<RecyclerView>(R.id.rv_most_liked)
-        val daftarJudul = arrayOf<String>("Surat Izin Sakit", "Surat Pengunduran Diri", "Surat Ingin Menikah")
-        val daftarKeterangan = arrayOf<String>("Ketika Mahasiswa tidak enak badan dan direkomendasikan pihak medis untuk beristirahat", "Ketika Capek Kuliah DO Solusinya", "Jomblo jangan tekan ini")
+        val daftarJudul =
+            arrayOf<String>("Surat Izin Sakit", "Surat Pengunduran Diri", "Surat Ingin Menikah")
+        val daftarKeterangan = arrayOf<String>(
+            "Ketika Mahasiswa tidak enak badan dan direkomendasikan pihak medis untuk beristirahat",
+            "Ketika Capek Kuliah DO Solusinya",
+            "Jomblo jangan tekan ini"
+        )
 
         val suratAdapter = SuratAdapter(daftarJudul, daftarKeterangan)
         recyclerViewItem.apply {
